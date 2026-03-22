24891a3cd10c3dd7f5b75ba2b1223643a4149eca
BURRA SRI PRAKASH <109139847+techhburner@users.noreply.github.com>
2026-03-22 17:48:02 +0530

    Delete fixstack directory

--- fixstack/lib/questions.ts
+++
@@ -1,7 +0,0 @@
-export type Question = {
-  id: number
-  title: string
-  code: string
-}
-
-export const questions: Question[] = []
--- fixstack/lib/supabase.ts
+++
@@ -1,7 +0,0 @@
-import { createClient } from "@supabase/supabase-js"
-
-const supabaseUrl = "https://hhqvpbigghbvzyqciudg.supabase.co"
-
-const supabaseKey = "sb_publishable_NkSCfxUAOv4CpCSfETvuPA_8X9z1r8M"
-
-export const supabase = createClient(supabaseUrl, supabaseKey)
